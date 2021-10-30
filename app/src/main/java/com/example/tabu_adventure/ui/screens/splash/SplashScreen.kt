28dce5b630185673ef99f0909f97cedd36c4fb39
Mustafa KARACABEY <mustafa.karacabey.engineer@gmail.com>
2021-10-30 19:26:32 +0300

    [Completed-Splash-Screen]

--- app/src/main/java/com/example/tabu_adventure/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/example/tabu_adventure/ui/screens/splash/SplashScreen.kt
@@ -3,11 +3,9 @@ package com.example.tabu_adventure.ui.screens.splash
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import com.example.tabu_adventure.R
 
 @Composable
@@ -15,7 +13,6 @@ fun SplashScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 40.dp, bottom = 40.dp)
     ) {
         Image(
             painter = painterResource(id = R.drawable.macera),
